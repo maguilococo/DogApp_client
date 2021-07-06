8101dbbd71d80045b5321e04db48ce5e68528908
maguilococo <56405247+maguilococo@users.noreply.github.com>
2021-07-06 06:59:12 -0300

    responsive bugs

--- src/components/BreedDetail/index.js
+++ src/components/BreedDetail/index.js
@@ -12,32 +12,35 @@ function BreedDetail({breed_detail, getBreedById, match }) {
         const idRaza = match.params.idRaza;
         getBreedById(idRaza);
     },[]); // eslint-disable-line
-    
+   
+    const temps = breed_detail.temperaments?.map(t => t.name).join(', ');
+
+
     // Renders the required details of the selected breed
     return(
         <div className={s.back}>
           <div className={s.container}>
             <div className={s.cover} style={{backgroundImage: `url(${breed_detail.image})`}}></div>
             <div className={s.content}>
-            <div className={s.close}>
-              <Link to='/breeds'><FaRegTimesCircle /></Link></div>
-              <div className={s.titles}>
-                <h2 className={s.title}>{breed_detail.name}</h2>
-                <h3 className={s.subtitle}>{breed_detail.temperament}</h3>
+              <div className={s.close}>
+                <Link to='/breeds'><FaRegTimesCircle /></Link></div>
+                <div className={s.titles}>
+                  <h2 className={s.title}>{breed_detail.name}</h2>
+                  <h3 className={s.subtitle}>{temps}</h3>
+                </div>
+                <div className={s.info}>
+                  <span>Height: </span>
+                  <h3>{breed_detail.height} inches</h3>
+                </div>
+                <div className={s.info}>
+                  <span>Weight: </span>
+                  <h3>{breed_detail.weight_show} pounds</h3>
+                </div>
+                <div  className={s.info}>
+                  <span>Life-span: </span>
+                  <h3>{breed_detail.years}</h3>
+                </div>
               </div>
-              <div className={s.info}>
-                <span>Height: </span>
-                <h3>{breed_detail.height} inches</h3>
-              </div>
-              <div className={s.info}>
-                <span>Weight: </span>
-                <h3>{breed_detail.weight_show} pounds</h3>
-              </div>
-              <div  className={s.info}>
-                <span>Life-span: </span>
-                <h3>{breed_detail.years}</h3>
-              </div>
-            </div>
           </div>
         </div>
     )
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FiMail } from "react-icons/fi";
 import style from "./Footer.module.css";
 
 function Footer() {
@@ -6,17 +8,19 @@ function Footer() {
     <div className={style.main}>
       <div className={style.ctn}>
         <div className={style.title}>
-            &copy;{new Date().getFullYear()} DogApp &nbsp;|&nbsp;<a href='https://github.com/maguilococo' className={style.link}>&nbsp;GitHub&nbsp;</a> | <a className={style.link} href="https://www.linkedin.com/in/magdalenalococo/">&nbsp;LinkedIn</a>
-            | <a className={style.link} href="mailto:magdalenalococo@gmail.com">&nbsp;Contacto</a>
+            <a href='https://github.com/maguilococo' rel="noopener noreferrer" target="_blank" className={style.link}>&nbsp;<FaGithub />&nbsp;</a> 
+            | <a className={style.link} href="https://www.linkedin.com/in/magdalenalococo/" rel="noopener noreferrer" target="_blank">&nbsp;<FaLinkedin /></a>
+            | <a className={style.link} href="mailto:magdalenalococo@gmail.com" rel="noopener noreferrer" target="_blank">&nbsp;<FiMail /></a>
+            | &nbsp;<span className={style.license}>&nbsp;&copy;{new Date().getFullYear()} DogApp &nbsp;</span>
         </div>
         <div className={style.disclaimer}>
           <p>
             This app was developed in React, Redux, CSS modules, HTML in the front and Node.js, Express, Sequelize and PostgreSQL in the server side.
           </p>
-          <p>You can visit both respositories at 
-            <a href="https://github.com/maguilococo/DogApp_client"> client</a>
+          <p>You can visit both respositories -> 
+            <a href="https://github.com/maguilococo/DogApp_client" rel="noopener noreferrer" target="_blank"> client</a>
             {' & '}
-            <a href="https://github.com/maguilococo/DogApp-api">server</a>
+            <a href="https://github.com/maguilococo/DogApp-api" rel="noopener noreferrer" target="_blank">server</a>
           </p>
         </div>
       </div>
